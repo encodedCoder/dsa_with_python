6a1343b3e522720a698548f94f4a55a74c3e0de0
Suresh <sureshrao100000@gmail.com>
2022-12-15 08:44:19 +0530

    Add online_stock_span to Stack and Queue Problems

---
+++ 14_Stack_Queue_Problems/05_Online_stock_span.py
@@ -0,0 +1,12 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        pass
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
