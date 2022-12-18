fc4372d6f3b31d10e7f8526998455ede1954229b
Suresh <sureshrao100000@gmail.com>
2022-12-18 12:08:58 +0530

    Add lowest_common_ancestor to Tree Problems

---
+++ 15_Trees_Problems/06_lowest_common_ancestor.py
@@ -0,0 +1,11 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# def lowestCommonAncestor(self, root, p, q: 'TreeNode') -> 'TreeNode':
+
+class Solution:
+    def lowestCommonAncestor(self, root):
+        pass
