e2338b849c3bfd926c40103342ecd0351485806c
Suresh <sureshrao100000@gmail.com>
2022-12-19 12:16:27 +0530

    Add findMedianInStream Approach_4 to Heap Problems

---
+++ 16_Heap_Problems/03_findMedianInStream_Approach_04.py
@@ -0,0 +1,20 @@
+# Approach_03 - AVL Tree
+
+class MedianFinder:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        
+
+    def addNum(self, num: int) -> None:
+        
+
+    def findMedian(self) -> float:
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
